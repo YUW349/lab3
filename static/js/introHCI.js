c5d2c09f92e9be00e1053b5b4370fe95f1c49b42
Yujie Wang <Yuw349@ucsd.edu>
2018-01-25 01:11:26 -0800

    javascript and css added

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -10,9 +10,38 @@ $(document).ready(function() {
  */
 function initializePage() {
 	$("#testjs").click(function(e) {
-		$('.jumbotron h1').text("Javascript is connected");
+		//step42
+		$("#testjs").text("Please wait...");
+		//step43
+		$(".jumbotron p").addClass("active");
 	});
 
 	// Add any additional listeners here
 	// example: $("#div-id").click(functionToCall);
+	$("a.thumbnail").click(projectClick);
 }
+
+function projectClick(e) { 
+    // prevent the page from reloading      
+    e.preventDefault();
+    // In an event handler, $(this) refers to      
+    // the object that triggered the event      
+    $(this).css("background-color", "#7fff00");
+
+    // lab3 step38 
+   // var containingProject = $(this).closest(".project");
+    //containingProject.append("<div class='project-description'><p>Description of the project.</p></div>");
+
+    // step40
+    var containingProject = $(this).closest(".project");
+    var description = $(containingProject).find(".project-description");
+    if (description.length == 0) {
+       $(containingProject).append("<div class='project-description'><p>Description of the project.</p></div>");
+    } else {
+       description.fadeToggle();//"<p>Stop clicking on me! You just did it at " + (new Date()) + "</p>"
+    }
+
+}
+
+ 
+
